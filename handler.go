a4337c816062c20591879b972551cfb5ceeaa10b
Joet <j.lil.boy116@gmail.com>
2026-07-17 20:23:22 +0300

    feat: add limit query parameter to input validation to /logs endpoint

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
@@ -17,8 +18,27 @@ func statHandler(w http.ResponseWriter, r *http.Request){
   w.Write([]byte("Signalcasket is running"))
 }
 func logsHandler(w http.ResponseWriter, r *http.Request){
+  limitStr := r.URL.Query().Get("limit")
+  limit := -1
+  if limitStr != ""{
+    n,err := strconv.Atoi(limitStr)
+  if err != nil{
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+ limit = n
+  }
+  
 mu.Lock()
-data,err := json.Marshal(logs)
+result := logs
+if limit > 0{
+  start := len(logs)-limit
+  if start <0 {
+    start = 0
+  }
+  result = logs[start:]
+}
+data,err := json.Marshal(result)
 mu.Unlock()
 if err != nil{
   w.WriteHeader(http.StatusInternalServerError)
